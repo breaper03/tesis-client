7625cc77c3083714b1336de1c4e28d32660d58a0
Daniel Dun <danieldum28@gmail.com>
2024-06-14 21:24:09 -0400

    Add  Tittle Nav

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -17,7 +17,15 @@ export const NavBar = () => {
 
   return (
     <div className='flex flex-row items-center justify-between w-full px-10 py-3 border-b'>
-      <span className='text-2xl font-semibold'>Title Here!!</span>
+     <div className="gap-2 flex items-center">
+        <span className="text-2xl text-white font-bold bg-blue-600 dark:bg-blue-700/80 py-1 px-2 rounded-lg">
+          Assist
+        </span>
+        <span className="text-2xl hidden sm:inline">
+          Control
+        </span>
+      </div>
+      
       <div className='flex items-center gap-4'>
         <span className='text-lg font-semibold'>{user ? `${user.firstname} ${user.lastname[0]}.` : ""}</span>
         <ThemeSwitch />
