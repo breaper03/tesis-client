b4a142c397e38b35b6a7095cf795344864b27826
breaper03 <gabrielpaez011@gmail.com>
2024-07-17 10:22:17 -0400

    Fix

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,24 +1,34 @@
-import  z } from 'zod';
+import { z } from "zod";
 
 export const UserSchema = z.object({
   id: z.string().trim(),
   _id: z.string().trim(),
   firstname: z.string().min(3).max(15),
   lastname: z.string().min(3).max(15),
-  document: z.string().trim().min(7).max(8).regex(/^[0-9]{7,8}$/),
+  document: z
+    .string()
+    .trim()
+    .min(7)
+    .max(8)
+    .regex(/^[0-9]{7,8}$/),
   password: z.string().min(8),
-  access: z.enum(['admin', 'worker']),
+  access: z.enum(["admin", "worker"]),
   createdAt: z.date(),
-  updatedAt: z.date()
+  updatedAt: z.date(),
 });
 export const CreateUserSchema = z.object({
   firstname: z.string().min(3).max(15),
   lastname: z.string().min(3).max(15),
-  document: z.string().trim().min(7).max(8).regex(/^[0-9]{7,8}$/),
+  document: z
+    .string()
+    .trim()
+    .min(7)
+    .max(8)
+    .regex(/^[0-9]{7,8}$/),
   password: z.string().min(8),
-  access: z.enum(['admin', 'worker']),
+  access: z.enum(["admin", "worker"]),
 });
 
 export type IUser = z.infer<typeof UserSchema>;
 
-export type ICreateUser = z.infer<typeof CreateUserSchema>
+export type ICreateUser = z.infer<typeof CreateUserSchema>;
